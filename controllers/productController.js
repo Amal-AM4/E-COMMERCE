0eec2b5616871d0cb2c0d9eda6b2dc30c39dfdcd
Amal A M <vectorhero007@gmail.com>
2024-04-11 14:21:26 +0530

    product display with category name with it

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,7 +4,11 @@ const prisma = new PrismaClient();
 
 async function productList (req, res) {
     try {
-        const productData = await prisma.product.findMany();
+        const productData = await prisma.product.findMany({
+            include: {
+                category: true //category    product[] @relation("CategoryList") in model schema
+            }
+        });
         res.render('product/productList', { data: productData });
     } catch (error) {
         console.error(error);
